cc5885851971a599d6b04774ba4618ceb2b4a03f
gustavolonda <gustavolonda@ingenieros.com>
2015-07-17 01:35:45 -0500

    Modificar el bean RssFeedReaderMB para leer los rss

--- src/main/java/com/so/rssfeedreader/bean/RssFeedReaderMB.java
+++ src/main/java/com/so/rssfeedreader/bean/RssFeedReaderMB.java
@@ -11,21 +11,23 @@ import com.so.rssfeedreader.entities.Feed;
 import com.so.rssfeedreader.entities.FeedMessage;
 import com.so.rssfeedreader.entities.Image;
 import com.so.rssfeedreader.entities.Item;
+import com.so.rssfeedreader.entities.ValoresEstaticos;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedProperty;
 
 /**
  *
- * @author ronny
+ * @author gustavo
  */
 public class RssFeedReaderMB {
 
     /**
      * Creates a new instance of RssFeedReaderMB
      */
-	private String title;
+   private String title;
     private String link;
     private String description;
     private String descriptionNoticia;
@@ -36,27 +38,62 @@ public class RssFeedReaderMB {
     private String urlImagen;
     private List<Item> entries = new ArrayList<Item>();
     private static final List<String> links = new ArrayList<String>();
-     private  List<Feed> listaFeed = new ArrayList<Feed>(listaFeed2);
+     private  List<Feed> listaFeed = new ArrayList<Feed>(ValoresEstaticos.listaFeed2);
      static final List<Feed> listaFeed2 = new ArrayList<Feed>();
     private Image image = new Image();
-	@ManagedProperty(value="#{rssfeedreaderBo}")
-	
-    private RssfeedreaderBo rssfeedreaderBo=new RssfeedreaderImpBo();
+ @ManagedProperty(value="#{rssfeedreaderBo}")
+     private RssfeedreaderBo rssfeedreaderBo=new RssfeedreaderImpBo();
  
+
     public void readFeed(){ 
+     
      Feed fee=new Feed(title,link,description,language,
                                 copyright,pubDate);
      
      fee=rssfeedreaderBo.readerFeed(link);
-     listaFeed2.add(fee);
+     ValoresEstaticos.listaFeed2.add(fee);
+     
+     
      listaFeed.clear();
-     listaFeed.addAll(listaFeed2);
+     listaFeed.addAll(ValoresEstaticos.listaFeed2);
        links.add(title);
         
-        
-          
+        setTitle(fee.getMessage().get(1).getTitle());
+       setDescription(fee.getDescription());
+       setEntries(fee.getMessage());
+       HashSet<Item> hashSet = new HashSet<Item>(getEntries());
+		getEntries().clear();
+		getEntries().addAll(hashSet);
+       setLanguage(fee.getLaunguage());
+       setPubDate(fee.getPubDate());
+       setCopyright(fee.getCopyright());
+       setImage(fee.getImage());
+       setUrlImagen(fee.getMessage().get(1).getUrlImagen());
+       setDescriptionNoticia(fee.getMessage().get(1).getDescription());
+      // System.out.print(fee.toString());
+       //System.err.print(rssfeedreaderBo.readerFeed(link).getEntries().get(0).getUrlImgen());
+       
+
+     
+     
+
+      
          
      }
+    public void iniciar(){ 
+        while(true){
+            new NewThread();
+    try {
+      // espera un tiempo para que terminen los otros hilos
+      Thread.sleep(3000);
+      System.out.printf("Hola");
+    } catch (InterruptedException e) {
+      System.out.println("Interrupcion del hilo principal");
+    }
+    System.out.println("Sale del hilo principal.");
+        
+        }
+    }
 
     public String getTitle() {
         return title;
@@ -153,8 +190,53 @@ public class RssFeedReaderMB {
     public  List<Feed> getListaFeed() {
         return listaFeed;
     }
+  @PostConstruct
+   public void init(){
+       new NewThread();
+    try {
+      // espera un tiempo para que terminen los otros hilos
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      System.out.println("Interrupcion del hilo principal");
+    }
+    System.out.println("Sale del hilo principal.");
+       
+   } 
+   
+  
+    
+}
+ class NewThread implements Runnable {
+ private  List<Feed> listaFeed = new ArrayList<Feed>(ValoresEstaticos.listaFeed2);
+ private  List<Feed> listaFeed2 = new ArrayList<Feed>(ValoresEstaticos.listaFeed2);
+ private RssfeedreaderBo rssfeedreaderBo=new RssfeedreaderImpBo();
+ public Thread t;
+  NewThread() {
+    
+    t = new Thread(this);
+    
+    t.start(); // Comienza el hilo
+    
+  }
+  // Este es el punto de entrada del hilo.
+  public void run() {
+      List<Feed> listaFeed3 = new ArrayList<Feed>();
+      Feed feed;
+    try {
 
-    
-    
-    
+      for(int i = 0; i > listaFeed.size(); i++) {
+        feed=new Feed();
+        feed=rssfeedreaderBo.readerFeed(listaFeed.get(i).getLink());
+        listaFeed2.add(feed);
+        Thread.sleep(10);
+      }
+      ValoresEstaticos.listaFeed2.clear();
+      ValoresEstaticos.listaFeed2.addAll(listaFeed2);
+    }
+
+    catch (InterruptedException e) {
+      System.out.println("Interrupcion del hilo hijo");
+    }
+    System.out.println("Sale del hilo hijo" );
+  }
 }
